21cf878929b00bc4f701b31b0d80a57a54916331
Megan Collier <meganraec@live.com>
2018-06-21 03:00:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable
-  
+
   attr_accessor :name
   attr_reader :artist
 
@@ -12,25 +12,28 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+#find_by_name now exists in the module Findable for use by all classes.
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+#reset_all now exists in the module Memorable for use by all classes.
+  # def self.reset_all
+  #   @@songs.clear
+  # end
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
+#This method has been moved to module Memorable for use by all classes. 
+  # def self.count
+  #   self.all.count
+  # end
 
   def self.all
     @@songs
