66903946740e268823f864d6147e6869b3c7e531
Megan Collier <meganraec@live.com>
2018-06-21 03:48:03 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,24 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+  extend Findable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+#find_by_name now exists in the module Findable for use by all classes.
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def initialize
-    @@artists << self
+    super
+    # self.class.all << self  This method moved to scope InstanceMethods within module Memorable.
     @songs = []
   end
 
@@ -19,13 +26,15 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+#reset_all now exists in the module Memorable for all classes.
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    @@artists.count
-  end
+#This method has been moved to module Memorable for use by all classes.
+  # def self.count
+  #   @@artists.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,8 +45,9 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#This method now exists in the Paramable module for use by all classes.
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,42 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+  extend Findable
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize This method now exists in the InstanceMethods scope of the Memorable module.
+  #   self.class.all << self
+  # end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+#find_by_name now exists in the module Findable for use by all classes.
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+#reset_all now exists in the module Memorable for use by all classes.
+  # def self.reset_all
+  #   @@songs.clear
+  # end
+#This method now exists in Paramable module for use by all classes.
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
-  end
+#This method has been moved to module Memorable for use by all classes.
+  # def self.count
+  #   self.all.count
+  # end
 
   def self.all
     @@songs
